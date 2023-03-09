6ba1d4eab5b324e98d21d877d0fd0e31b5c9721c
Shaf8808 <shadmankhan08@gmail.com>
2023-03-09 13:59:50 +0000

    Correct syntax errors for form.js using jshint

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,12 +1,14 @@
+/*jshint esversion: 6 */
+
 /* The code in this file was written with some help from Sam 
 Timmins' quiz app */
 
-const startButton = document.getElementById('start-btn')
+const startButton = document.getElementById('start-btn');
 
 // Welcome section
 
-const welcomeMessageRef = document.getElementById('welcome-message')
-const welcomeSection = document.getElementById('welcome')
+const welcomeMessageRef = document.getElementById('welcome-message');
+const welcomeSection = document.getElementById('welcome');
 
 
 // User form section
@@ -19,16 +21,16 @@ const userFormOpen = () => {
     welcomeMessageRef.innerHTML = '';
     userNameRef.value = '';
     userNameRef.focus();
-}
+};
 
 const handleUserFormSubmit = event => {
     event.preventDefault();
-    userSectionRef.classList.add('hidden')
-    welcomeSection.classList.remove('hidden')
+    userSectionRef.classList.add('hidden');
+    welcomeSection.classList.remove('hidden');
     welcomeMessageRef.innerHTML = `Are you ready ${userNameRef.value}?`;
-    localStorage.setItem('userName', userNameRef.value)
-    startButton.classList.remove('hidden')
-}
+    localStorage.setItem('userName', userNameRef.value);
+    startButton.classList.remove('hidden');
+};
 
 // User form submit
 userFormRef.addEventListener('submit', handleUserFormSubmit);
