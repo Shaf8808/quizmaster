f15ed7bdcf45dd63c4c01676dc9ab93cc190e889
Shaf8808 <shadmankhan08@gmail.com>
2023-03-09 13:41:40 +0000

    Correct syntax by using jshint

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,18 @@
+/*jshint esversion: 6 */
+
+
 /* The code in this file was written with the help of
 James Q Quick's video tutorial of making a quiz app */
 
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-const homeButton = document.getElementById('home-btn')
-const startButton = document.getElementById('start-btn')
+const homeButton = document.getElementById('home-btn');
+const startButton = document.getElementById('start-btn');
 
 
 // Question counter and score variables
-const questionCounterText = document.getElementById('questionCounter')
-const scoreText = document.getElementById('score')
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -25,16 +28,12 @@ let availableQuestions = [];
 let questions = [];
 
 fetch("questions.json").then(res => {
-return res.json()
-})
-.then(loadedQuestions => {
-    questions = loadedQuestions
-    startGame()
-})
-// Logs any error that may occur while playing or loading up the quiz
-.catch(err => {
-    console.log(err)
-})
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        questions = loadedQuestions;
+        startGame();
+    });
 
 // Number of points added each time the user is correct and number of questions for question counter
 
@@ -45,12 +44,12 @@ const maxQuestions = 10;
 filling in their details */
 
 startGame = () => {
-    questionCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    console.log(availableQuestions)
-    getNewQuestion()
-}
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+};
 
 
 /**
@@ -62,54 +61,54 @@ getNewQuestion = () => {
 
     if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
         // Stores final score of user for it to be accessed on end screen
-        localStorage.setItem('mostRecentScore', score)
-        return window.location.assign("end.html")
+        localStorage.setItem('mostRecentScore', score);
+        return window.location.assign("end.html");
     }
 
     // Dynamically updates question counter depending on question user is currently on
-    questionCounter++
-    questionCounterText.innerText = `${questionCounter}/${maxQuestions}`
+    questionCounter++;
+    questionCounterText.innerText = `${questionCounter}/${maxQuestions}`;
 
     /* This takes a random number from my questions array and 
     displays it as the current question that the user is on. */
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionIndex]
-    question.innerText = currentQuestion.question
-    
-    /* Adds a number dataset in order to distinguish the correct option
-    from the others */ 
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
 
-    availableQuestions.splice(questionIndex, 1)
+    /* Adds a number dataset in order to distinguish the correct option
+    from the others */
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
 
     /* Adds the true boolean value if the user selects the correct option */
 
-    acceptingAnswers = true
-}
+    acceptingAnswers = true;
+};
 
 /* Returns answers as "false" if the user selects the wrong option */
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
-        if (!acceptingAnswers) return
+        if (!acceptingAnswers) return;
 
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
 
         // Adds incorrect and correct colour to specific option chosen by the user
 
         const classToApply =
-            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect"
+            selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-            // Dynamically adds the correctScore value by calling incrementScore function
-            if(classToApply === 'correct') {
-                incrementScore(correctScore)
-            }
-        selectedChoice.parentElement.classList.add(classToApply)
+        // Dynamically adds the correctScore value by calling incrementScore function
+        if (classToApply === 'correct') {
+            incrementScore(correctScore);
+        }
+        selectedChoice.parentElement.classList.add(classToApply);
 
 
         /**
@@ -117,18 +116,17 @@ choices.forEach(choice => {
          * 2 second delay and removes the class of correct and incorrect
          */
         setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-        }, 2000)
-    })
-})
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 2000);
+    });
+});
 
 /**
  * Updates score number depending on the user's choice
  * once it is called
  */
 incrementScore = num => {
-    score +=num
-    scoreText.innerText = score
-}
-
+    score += num;
+    scoreText.innerText = score;
+};
